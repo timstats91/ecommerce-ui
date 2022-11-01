fc8b3c8a848262b18fa9873d6b15fbb17a6fff0c
timstats91 <timstats91@gmail.com>
2022-11-01 13:10:28 -0500

    adding routes to web history

--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,58 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/admin/categories/add',
-    name: 'addCategory',
-    component: () => import('../views/Category/AddCategoryView.vue')
-  },
-  {
-    path: '/admin/categories/:id',
-    name: 'editCategory',
-    component: () => import('../views/Category/EditCategoryView.vue')
-  },
-  {
-    path: '/admin/categories',
-    name: 'allCategories',
-    component: () => import('../views/Category/CategoryView.vue')
-  },
-  {
-    path: '/admin/products/add',
-    name: 'addProduct',
-    component: () => import('../views/Product/AddProductView.vue')
-  },
-  {
-    path: '/admin/products/:id',
-    name: 'editProduct',
-    component: () => import('../views/Product/EditProductView.vue')
-  },
-  {
-    path: '/admin/products',
-    name: 'allProducts',
-    component: () => import('../views/Product/ProductView.vue')
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    component: () => import('../views/AdminView.vue')
-  },
-]
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    },
+    {
+      path: '/admin/categories/add',
+      name: 'addCategory',
+      component: () => import('../views/Category/AddCategoryView.vue')
+    },
+    {
+      path: '/admin/categories/:id',
+      name: 'editCategory',
+      component: () => import('../views/Category/EditCategoryView.vue')
+    },
+    {
+      path: '/admin/categories',
+      name: 'allCategories',
+      component: () => import('../views/Category/CategoryView.vue')
+    },
+    {
+      path: '/admin/products/add',
+      name: 'addProduct',
+      component: () => import('../views/Product/AddProductView.vue')
+    },
+    {
+      path: '/admin/products/:id',
+      name: 'editProduct',
+      component: () => import('../views/Product/EditProductView.vue')
+    },
+    {
+      path: '/admin/products',
+      name: 'allProducts',
+      component: () => import('../views/Product/ProductView.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/AdminView.vue')
+    },
+  ]
 })
 
 export default router
